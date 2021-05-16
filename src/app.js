976be4a4da9c03eb22877572042367024329a11c
Estilicious <81353867+Estilicious@users.noreply.github.com>
2021-05-16 09:50:05 +0900

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ function displayTemperature (response){
     console.log(response.data);
     let temperatureElement=document.querySelector("#temperature");
     let cityElement=document.querySelector("#city");
+    let descriptionElement=document.querySelector("#description");
+    let humidityElement=document.querySelector("#humidity");
+    let windElement=document.querySelector("#wind");
     temperatureElement.innerHTML=Math.round(response.data.main.temp);
     cityElement.innerHTML=response.data.name;
+    descriptionElement.innerHTML=response.data.weather[0].description;
+    humidityElement.innerHTML=response.data.main.humidity;
+    windElement.innerHTML=Math.round(response.data.wind.speed);
 }
 
 let apiKey="6ee93f63f08e0073ad97e7e8155d775c";
