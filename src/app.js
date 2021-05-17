f5fed9a06a5cff57e48340f64284b54930d09a01
Estilicious <81353867+Estilicious@users.noreply.github.com>
2021-05-17 20:41:30 +0900

    Updated styling

--- src/app.js
+++ src/app.js
@@ -14,12 +14,19 @@ function formatDate(timestamp){
 }
 function displayTemperature (response){
     let temperatureElement=document.querySelector("#temperature");
+    
     let cityElement=document.querySelector("#city");
+    
     let descriptionElement=document.querySelector("#description");
+    
     let humidityElement=document.querySelector("#humidity");
+    
     let windElement=document.querySelector("#wind");
+    
     let dateElement=document.querySelector("#date");
+    
     let iconElement=document.querySelector("#icon");
+    
     celsiusTemperature=response.data.main.temp;
     
     temperatureElement.innerHTML=Math.round(celsiusTemperature);
@@ -31,29 +38,36 @@ function displayTemperature (response){
     iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt",response.data.weather[0].description);
 }
+
 function search(city){
 let apiKey="6ee93f63f08e0073ad97e7e8155d775c";
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
 }
+
 function handleSubmit(event){
     event.preventDefault();
     let cityInputElement=document.querySelector("#city-input");
     search (cityInputElement.value);
 }
+
 function displayFahrenheitTemperature(event){
     event.preventDefault();
     let temperatureElement=document.querySelector("#temperature");
+    
     //Remove the active class from the celsius link
     celsiusLink.classList.remove("active");
      fahrenheitLink.classList.add("active");
-    let fahrenheitTemperature=(celsiusTemperature * 9) / 5+32;
+    
+     let fahrenheitTemperature=(celsiusTemperature * 9) / 5+32;
     temperatureElement.innerHTML=Math.round(fahrenheitTemperature);
 }
 function displaycelsiusTemperature(event){
     event.preventDefault();
     celsiusLink.classList.add("active");
     fahrenheitLink.classList.remove("active");
+
+    
     let temperatureElement=document.querySelector("#temperature");
     temperatureElement.innerHTML=Math.round(celsiusTemperature);
 }
